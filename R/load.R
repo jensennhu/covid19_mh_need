78b43ff6028ea6b66783ee4417e7a17961a69e05
Jensen Hu <jensennhu@gmail.com>
2020-05-12 16:24:23 -0400

    flexdashboard update, added likely impacted schools

--- R/load.R
+++ R/load.R
@@ -13,6 +13,8 @@ nyc_zips <- read_csv("../RawData/nyc_zips.csv")
 child_opp <- read_csv("../RawData/ny_child_opp.csv")
 covid <- read_csv("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/tests-by-zcta.csv")
 schools <- read.socrata("https://data.cityofnewyork.us/resource/wg9x-4ke6.json")
+lcgms <- read_csv("../RawData/LCGMS_SchoolData_20200509_1135.csv")
+school_tracts <- read_csv("../RawData/school tract.csv")
 # tigris NYS county spatial data
 options(tigris_use_cache = TRUE)
 county_sp <- counties(state="NY")
@@ -55,10 +57,51 @@ child_opp$c5_COI_nat <- ordered(child_opp$c5_COI_nat, levels=c("Very Low", "Low"
 merge_opp <- geo_join(tracts, child_opp, "GEOID", "GEOID")
 merge_opp <- merge_opp[merge_opp@data$countyfips %in% nyc_cfips,] # only NYC County FIPS
 
+# merge svi and coi 
+svi_opp <- left_join(svi_tracts@data, merge_opp@data, by="GEOID")
+
 # lat lng convert to numeric
 schools$longitude <- as.numeric(schools$longitude)
 schools$latitude <- as.numeric(schools$latitude)
+schools <- schools %>% filter(status_descriptions == "Open") # 1900 open schools
+# identify schools in high impact regions through census tract
+lcgms <- lcgms[c(1,13:18)]
+colnames(lcgms)[1] <- "system_code"
+schools <- left_join(schools, lcgms, by="system_code") # 2190 schools
+schools <- tibble::rowid_to_column(schools, "index")
+school_tracts <- left_join(schools, school_tracts, by="index")
 
+# update partial tracts 
+school_tracts$truetract <- ifelse(school_tracts$Census.Tract == school_tracts$key1, school_tracts$key1, school_tracts$key1)
+# replace na values in updated track var w/older track var
+school_tracts$truetract <- coalesce(school_tracts$key1, school_tracts$Census.Tract)
+# only open and partial tract available schools n = 1899
+school_tracts <- school_tracts %>% filter(status_descriptions == "Open" & !is.na(truetract))
+# recreate full census tract to connect to SVI_OPP data
+school_tracts$cnty_code <- paste0(school_tracts$state, "0", school_tracts$cnty)
+school_tracts$tract_len <- lapply(school_tracts$truetract, nchar)
+trans_tract <- function(x){
+  if (x == 2) {
+    result <- "0000"
+  }
+  else if (x == 3) {
+    result <- "000"
+  }
+  else if (x == 4) {
+    result <- "00"
+  }
+  else if (x == 5) {
+    result <- "0"
+  }
+  else {
+    result <- ""
+  }
+  return(result)
+}
+# add 
+school_tracts$y <- lapply(school_tracts$tract_len, trans_tract)
+school_tracts$GEOID <- paste0(school_tracts$cnty_code, school_tracts$y, school_tracts$truetract)
+school_tracts <- school_tracts[c(3, 25, 26, 33, 43, 46, 61)]
 
 # Save RDS
 saveRDS(schools, file = "rds_files/schools.rds")
@@ -67,7 +110,8 @@ saveRDS(nyc_covid, file = "rds_files/nyc_covid.rds")
 saveRDS(nyc_zips, file = "rds_files/nyc_zips.rds")
 saveRDS(county_sp, file = "rds_files/county_sp.rds")
 saveRDS(svi_tracts, file = "rds_files/svi_tracts.rds")
+saveRDS(svi_opp, file = "rds_files/svi_opp.rds")
 saveRDS(ds_svi, file = "rds_files/ds_svi.rds")
-
+saveRDS(school_tracts, file = "rds_files/school_tracts.rds")
 
 
